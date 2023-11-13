fcd93eea000f79db2fa83fe6ac6ae9493541324f
v1p3r75 <elfriedv16@gmail.com>
2023-11-13 23:20:37 +0100

    Add welcome page

--- src/views/welcome.blade.php
+++ src/views/welcome.blade.php
@@ -1,11 +1,143 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Floky | Welcome</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+    <style>
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        ::-webkit-scrollbar {
+            width: 5px;
+        }
+
+        ::-webkit-scrollbar-track {
+            border-radius: 5px;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: #565f89;
+            border-radius: 10px;
+        }
+
+        /* ::-webkit-scrollbar-thumb:hover{background: yellowgreen;} */
+
+        body {
+            font-family: "Montserrat", "Arial";
+            padding: 0;
+            margin: 0;
+            color: white;
+            overflow-y: hidden;
+        }
+
+        #page {
+            position: relative;
+            height: 100vh;
+            padding: 10px 5%;
+            background: #181823;
+            overflow-y: scroll;
+        }
+
+        .d-none {
+            display: none;
+        }
+
+        a {
+
+            display: inline-block;
+            text-transform: uppercase;
+            color: #E9F8F9;
+            text-decoration: none;
+            border: 2px solid;
+            background: transparent;
+            padding: 10px 40px;
+            font-size: 14px;
+            font-weight: 700;
+        }
+
+        .features {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            gap: 20px;
+            margin: 5rem 0;
+        }
+
+        .feature {
+            width: 45%;
+            min-height: 150px;
+            padding: 20px;
+            background-color: white;
+            color: rgba(0, 0, 0, 0.7);
+            scrollbar-width: 10px;
+            scrollbar-color: white blue;
+            border-radius: 10px;
+            gap: 30px;
+        }
+
+        .feature div {
+            align-self: center;
+        }
+
+        .feature i {
+            font-size: 2rem;
+        }
+
+        .feature h3 {
+            margin: 20px 0 0 0;
+        }
+    </style>
+
 </head>
+
 <body>
-    <h1>Welcome To {{ $name }}</h1>
+    <div id="page">
+        <div class="header" style="display: flex; justify-content: space-between; padding: 8px">
+            <h1 class="brands">Floky</h1>
+            <h4><a href="https://github.com/v1p3r75/Floky" target="_blank" class="help">Get Started</a></h4>
+        </div>
+        <div class="features">
+            <div class="feature">
+                <div><i class="icon">🚀</i></div>
+                <div>
+                    <h3>Fast and Efficient</h3>
+                    <p>Our framework is designed for speed and efficiency in web development.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div><i class="icon">🎨</i></div>
+                <div>
+                    <h3>Flexible Design</h3>
+                    <p>Create beautiful and flexible web applications with ease.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div>
+                    <i class="icon">🧩</i>
+                </div>
+                <div>
+                    <h3>Modular Architecture</h3>
+                    <p>Build applications with a clean and modular code structure.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div><i class="icon">🌱</i></div>
+                <div>
+                    <h3>Lightweight</h3>
+                    <p>Our framework is lightweight and optimized for smaller projects, making it perfect for quick and efficient development.</p>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p style="font-size: 13px;"><i>Copyright &copy; {{ date('Y') }} - Floky<i></p>
+        </footer>
+    </div>
 </body>
+
 </html>
