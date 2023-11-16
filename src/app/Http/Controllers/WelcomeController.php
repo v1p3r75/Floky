1462fa4389c9ecea3cd28f6c0ab6c078887bd694
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-11-16 18:11:50 +0100

    init php-validator for test

--- src/app/Http/Controllers/WelcomeController.php
+++ src/app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use BlakvGhost\PHPValidator\Rules\RuleInterface;
 use BlakvGhost\PHPValidator\Validator;
 use Floky\Facades\Email;
 use Floky\Http\Controllers\Controller;
@@ -10,10 +11,6 @@ use Floky\Http\Requests\Request;
 class WelcomeController extends Controller
 {
 
-    private $rules = [
-        'email' =>'required|string|max_length:200',
-        'username' =>'required|max_length:100',
-    ];
 
     public function index(Request $request, Email $mail, $id) {
 
@@ -26,11 +23,39 @@ class WelcomeController extends Controller
     }
     
     public function validate(Request $request) {
-        $validate = new Validator($request->all(), $this->rules);
+        $rules = [
+            'email' =>'required|string|max_length:200',
+            'username' =>'required|max_length:100',
+            'password' => ['required','max_length:200', new SameRule()],
+        ];
+
+        $validate = new Validator($request->all(), $rules);
 
         if (!$validate->isValid()) {
             return dd($validate->getErrors());
         }
         return dd($validate->isValid());
+    }
+}
+
+class SameRule implements RuleInterface
+{
+    protected $field;
+
+    public function __construct(protected array $parameters = [])
+    {
+        
+    }
+
+    public function passes(string $field, $value, array $data): bool
+    {
+        $this->field = $field;
+
+        return dd($data);
+    }
+
+    public function message(): string
+    {
+        return "";
     }
 }
